8f59a4e508ac3bf52e3669fac029bb0f4fce6e02
Randhaja Malshan R. Wanigasekara <randhajapvt@gmail.com>
2016-11-17 18:53:21 +0530

    basic file

---
+++ administrator/logs/error.php
@@ -0,0 +1,17 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-11-11 14:06:57 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-11-11T14:06:57+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-14T17:00:14+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-14T18:05:14+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-14T18:05:25+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-14T18:18:35+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-14T19:15:16+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-15T07:29:15+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-15T07:46:55+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-15T08:03:25+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-15T08:27:20+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-15T09:31:46+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
